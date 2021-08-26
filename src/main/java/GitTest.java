0eee82a81e71a6d0145575f10bde5d7c29277a45
root <root@qq.com>
2021-08-26 17:45:17 +0800

    5

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master hello git3");
         System.out.println("2fenzhi hello git3");
+        System.out.println("push test");
     }
 }
